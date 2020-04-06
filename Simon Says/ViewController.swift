6f23e298a1d5b3938f7f91d7193583af0a4b44ec
Brandon Escobar <bescobar@bsd220.org>
2020-04-06 12:14:47 -0500

    Initial Commit

---
+++ Simon Says/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simon Says
+//
+//  Created by Brandon Escobar on 4/6/20.
+//  Copyright © 2020 Brandon Escobar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
