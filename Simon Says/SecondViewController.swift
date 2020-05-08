f9f3a6f44b7cd33a96d2f6cdda52d7ed08246ee6
Brandon Escobar <bescobar@bsd220.org>
2020-05-08 11:21:21 -0500

    Managed to have the buttons working.  This includes buttons tapping and waiting.

--- Simon Says/SecondViewController.swift
+++ Simon Says/SecondViewController.swift
@@ -10,10 +10,16 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
-    @IBOutlet weak var tapRed: UIView!
-    @IBOutlet weak var tapYellow: UIView!
-    @IBOutlet weak var tapGreen: UIView!
-    @IBOutlet weak var tapBlue: UIView!
+    @IBOutlet weak var tapRed: ViewGrid!
+    @IBOutlet weak var tapYellow: ViewGrid!
+    @IBOutlet weak var tapGreen: ViewGrid!
+    @IBOutlet weak var tapBlue: ViewGrid!
+    @IBOutlet weak var gameOverLabel: UILabel!
+    @IBOutlet weak var results: UILabel!
+    @IBOutlet weak var tryAgain: UIButton!
+    var colorsArray = [ViewGrid]()
+    var yourTurn = false
+    var gameGoing = true
     var guess = String()
     var timer = Timer()
     var second = 0
@@ -22,6 +28,14 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        colorsArray.append(tapRed)
+        colorsArray.append(tapYellow)
+        colorsArray.append(tapGreen)
+        colorsArray.append(tapBlue)
+        randomizePattern()
+        randomizePattern()
+        randomizePattern()
+        randomizePattern()
         randomizePattern()
         randomizePattern()
         randomizePattern()
@@ -46,10 +60,12 @@ class SecondViewController: UIViewController {
     }
     
     func tellPattern() {
+        var count = 1
         for i in answers {
+            yourTurn = false
             if second == 0 {
                 timer = Timer.scheduledTimer(withTimeInterval: 0, repeats: false, block: { (_) in
-                    print(i)
+                    print("SPACE ----- SPACE")
                 })
                 second += 1
             }
@@ -61,8 +77,11 @@ class SecondViewController: UIViewController {
                 print("1")
                 second += 1
                 timer = Timer.scheduledTimer(withTimeInterval: TimeInterval(second), repeats: false, block: { (_) in
-                    print(i)
                     self.tapRed.backgroundColor = UIColor.init(red: 128 / 255, green: 0 / 255, blue: 0 / 255, alpha: 1)
+                    if count == self.answers.count {
+                        self.yourTurn = true
+                    }
+                    count += 1
                 })
             } else if i == "Yellow"{
                 timer = Timer.scheduledTimer(withTimeInterval: TimeInterval(second), repeats: false, block: { (_) in
@@ -73,6 +92,10 @@ class SecondViewController: UIViewController {
                 second += 1
                 timer = Timer.scheduledTimer(withTimeInterval: TimeInterval(second), repeats: false, block: { (_) in
                     self.tapYellow.backgroundColor = UIColor.init(red: 128 / 255, green: 128 / 255, blue: 0 / 255, alpha: 1)
+                    if count == self.answers.count {
+                        self.yourTurn = true
+                    }
+                    count += 1
                 })
             } else if i == "Green"{
                 timer = Timer.scheduledTimer(withTimeInterval: TimeInterval(second), repeats: false, block: { (_) in
@@ -83,6 +106,10 @@ class SecondViewController: UIViewController {
                 second += 1
                 timer = Timer.scheduledTimer(withTimeInterval: TimeInterval(second), repeats: false, block: { (_) in
                     self.tapGreen.backgroundColor = UIColor.init(red: 0 / 255, green: 128 / 255, blue: 0 / 255, alpha: 1)
+                    if count == self.answers.count {
+                        self.yourTurn = true
+                    }
+                    count += 1
                 })
             } else if i == "Blue"{
                 timer = Timer.scheduledTimer(withTimeInterval: TimeInterval(second), repeats: false, block: { (_) in
@@ -93,14 +120,48 @@ class SecondViewController: UIViewController {
                 second += 1
                 timer = Timer.scheduledTimer(withTimeInterval: TimeInterval(second), repeats: false, block: { (_) in
                     self.tapBlue.backgroundColor = UIColor.init(red: 0 / 255, green: 0 / 255, blue: 128 / 255, alpha: 1)
+                    if count == self.answers.count {
+                        self.yourTurn = true
+                    }
+                    count += 1
                 })
             }
             second += 1
         }
     }
     
+    @IBAction func tapOnRed(sender: UITapGestureRecognizer) {
+        if yourTurn {
+            print("Red")
+        }
+    }
+    
+    @IBAction func tapOnYellow(sender: UITapGestureRecognizer) {
+        if yourTurn {
+            print("Yellow")
+        }
+    }
+    
+    @IBAction func tapOnGreen(sender: UITapGestureRecognizer) {
+        if yourTurn {
+            print("Green")
+        }
+    }
+    
+    @IBAction func tapOnBlue(sender: UITapGestureRecognizer) {
+        if yourTurn {
+            print("Blue")
+        }
+    }
+    
     func gameOver() {
-        //
+        tapRed.backgroundColor = .black
+        tapYellow.backgroundColor = .black
+        tapGreen.backgroundColor = .black
+        tapBlue.backgroundColor = .black
+        gameOverLabel.textColor = .black
+        answers = []
+        results.textColor = .black
     }
     
 }
---
+++ Simon Says/ViewGrid.swift
@@ -0,0 +1,13 @@
+//
+//  ViewGrid.swift
+//  Simon Says
+//
+//  Created by Brandon Escobar on 4/21/20.
+//  Copyright © 2020 Brandon Escobar. All rights reserved.
+//
+
+import UIKit
+
+class ViewGrid: UIView {
+    var canTap = true
+}
